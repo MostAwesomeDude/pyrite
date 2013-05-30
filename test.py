fa00159c906e97a40f0f6dd6741a412a2cb40a87
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-05-29 23:19:54 -0700

    Update tests.

--- test.py
+++ test.py
@@ -1,12 +1,30 @@
-import unittest
+from unittest import TestCase
 
 from pyrite import Munger
+from udp import pack, unpack
 
-class TestTV(unittest.TestCase):
+
+class TestPack(TestCase):
+
+    def test_single(self):
+        i = {"key": "value"}
+        o = "key=value"
+        self.assertEqual(pack(i), o)
+
+
+class TestUnpack(TestCase):
+
+    def test_single(self):
+        i = "key=value"
+        o = {"key": "value"}
+        self.assertEqual(unpack(i), o)
+
+
+class TestTV(TestCase):
 
     def test_community(self):
         s = "Community.S04E03.HDTV.x264-LOL.mp4"
-        name, ep, ext = Munger(s).filename()
+        name, ep, ext = Munger(s).tv()
 
         self.assertEqual(name, "Community")
         self.assertEqual(ep, (4, 3))
@@ -14,7 +32,7 @@ class TestTV(unittest.TestCase):
 
     def test_parks(self):
         s = "Parks.and.Recreation.S05E12.HDTV.x264-LOL.mp4"
-        name, ep, ext = Munger(s).filename()
+        name, ep, ext = Munger(s).tv()
 
         self.assertEqual(name, "Parks and Recreation")
         self.assertEqual(ep, (5, 12))
