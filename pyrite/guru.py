314523b3a28d1ba8d84657fa63e12e85a078184c
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-09-07 12:54:42 -0700

    guru: Hack up a guru for OSDB.

--- pyrite/guru.py
+++ pyrite/guru.py
@@ -2,6 +2,7 @@ from twisted.internet.defer import fail, inlineCallbacks
 from zope.interface import Interface, implements
 
 from pyrite.anidb import make_protocol
+from pyrite.osdb import OSDB, derphash
 from pyrite.hashing import size_and_hash
 
 
@@ -41,5 +42,40 @@ class AniDBGuru(object):
         if self._p:
             data = size_and_hash(filepath)
             return self._p.lookup(*data)
+
+        return fail("Not logged in!")
+
+
+class OSDBGuru(object):
+
+    implements(IGuru)
+
+    _db = None
+
+    def start(self, reactor, username, password):
+        # Annoyingly, the XML-RPC Proxy doesn't parameterize the reactor.
+        self._db = OSDB()
+        return self._db.login(username, password)
+
+    def stop(self):
+        if self._db:
+            d = self._db.logout()
+            @d.addCallback
+            def cb(chaff):
+                self._db = None
+            return d
+
+        return fail("Not logged in!")
+
+    def lookup(self, filepath):
+        if self._db:
+            derp = derphash(filepath)
+            d = self._db.search(derp)
+
+            @d.addCallback
+            def cb(data):
+                if len(data) > 1:
+                    raise Exception("Too many entries")
+                return data[0]
 
         return fail("Not logged in!")
